b5574cb8c0f3e5b1d7d3dad9f12de3340584448c
João Pinho <jvopinho.dev@gmail.com>
2022-06-16 23:13:48 -0300

    Chaging `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import ObjectManager from './utils/ObjectManager';
 import ObjectReference from './utils/ObjectReference';
+import Database from './Sydb';
+import { SydbOptions } from '../typings';
 
-export {
-    ObjectManager,
-    ObjectReference,
+function Sydb(options: SydbOptions|string) {
+    return new Database(options);
 }
+
+Sydb.Database = Database;
+Sydb.ObjectManager = ObjectManager;
+Sydb.ObjectReference = ObjectReference;
+Sydb.Sydb = Database;
+
+export = Sydb;
